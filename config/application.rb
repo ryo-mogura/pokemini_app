d52c141a00ae952abd70ebdc0fc700df837a8dd1
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-03 18:48:45 +0900

    [feat] #1 ジェネレーターのファイル作成制限

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Pokemini_app
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    g.skip_routes true
+    g.helper false
+    g.test_framework false
   end
 end
