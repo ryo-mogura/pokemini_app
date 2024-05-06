729a68a7c99abaa96b584e46f3b65b3e05c584f9
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-06 12:10:54 +0900

    [feat] #9 Poke APIで取得するコードをcontrollerからseed.rbに変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faraday'
+require 'json'
+
+pokemon_ids = [1, 4, 7, 152, 155, 158, 252, 255, 258, 387, 390, 393, 495, 498, 501, 650, 653, 656, 722, 725, 728, 810, 813, 816, 906, 909, 912]
+
+pokemon_ids.each do |id|
+  response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{id}"
+  fetched_pokemon = JSON.parse(response.body)
+
+  species_response = Faraday.get fetched_pokemon["species"]["url"]
+  species_data = JSON.parse(species_response.body)
+
+  ja_name = species_data["names"].find { |v| v["language"]["name"] == "ja" }["name"]
+
+  Pokemon.find_or_create_by(name: ja_name) do |pokemon|
+    pokemon.sprites = fetched_pokemon["sprites"].to_json
+  end
+end
